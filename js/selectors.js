486a51d5ca78c70b34bc732804533e5d574e08fd
Keith Jeter <kjeter2001@yahoo.com>
2018-01-18 04:52:38 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -19,3 +19,23 @@
 // function firstListItem() {
 // return $('.pics #pic-list li:first-child');
 // }
+
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem(){
+  return $('#pic-list li:first-child')
+}
