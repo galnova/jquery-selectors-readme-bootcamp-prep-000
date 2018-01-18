680e7a83e3365c2cdfe4a6276edde47850ad1652
Keith Jeter <kjeter2001@yahoo.com>
2018-01-18 04:36:49 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,31 @@
 // declare your functions here...
+
+function paragraphSelector () {
+  $('p');
+  
+  return;
+}
+
+function lastImageSelector() {
+  $('.pics img:last-child');
+  
+  return;
+}
+
+function ninjaBabySelector() {
+  $('#baby-ninja');
+  
+  return;
+}
+
+function divSelector() {
+  $('.pics');
+  
+  return;
+}
+
+function firstListItem() {
+  $('.pics #pic-list li:first-child');
+  
+  return;
+}
